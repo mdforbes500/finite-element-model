36c1562acafae6788e982b8a324ec70311a59241
mdforbes500 <mforbes4@gmu.edu>
2017-02-23 15:10:23 -0500

    Work on Problem 1
    Edited the stiffness matrix  function, created a general discretisation function, a script file for problem 1, and started on an approximation function which is buddy AF

--- stiffness_matrix.m
+++ stiffness_matrix.m
@@ -5,9 +5,9 @@ function K_e = stiffness_matrix(type, a_e, c_e, h_e)
  
  switch type
  case 1
-  K_e = a_e/h_*.[1 -1; -1 1] + c_e*h_e/6*.[2 1; 1 2];
+  K_e = a_e/h_e.*[1 -1; -1 1] + c_e*h_e/6.*[2 1; 1 2];
  case 2
-  K_e = a_e/(3*h_e)*.[7 -8 1; -8 16 -8; 1 -8 7] + c_e*h_e/30*.[4 2 -1; 2 16 2; -1 2 4];
+  K_e = a_e/(3*h_e).*[7 -8 1; -8 16 -8; 1 -8 7] + c_e*h_e/30.*[4 2 -1; 2 16 2; -1 2 4];
  end
 
 end
