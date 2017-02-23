c7a5a1f33974c6515c9cb998fd90e7d2ef6c25f3
mdforbes500 <mforbes4@gmu.edu>
2017-02-22 19:58:50 -0500

    Added quadratic shape functions
    Added equations for phi1, phi2, and phi3.

--- Quadratic_shape.m
+++ Quadratic_shape.m
@@ -3,9 +3,9 @@ function y = Quadratic_shape(x, x1, x2, x3, y1, y2, y3)
 %	Creates three local variables- phi1, ph2, and phi3- that represent the shape
 %	functions usedin the Galerkin method of approximation.
 %	These are used to define a function, y.
-phi_1 = 
-phi_2 =
-phi_3 =
+phi_1 = ((x2 - x)*(x3 - x))/((x2 - x1)*(x3 - x1));
+phi_2 = ((x1 - x)*(x3 - x))/((x1 - x2)*(x3 - x2));
+phi_3 = ((x1 - x)*(x2 - x))/((x1 - x3)*(x2 - x3));
 
 y = phi_1*y1 + phi_2*y2 + phi_3*y3;
 end
