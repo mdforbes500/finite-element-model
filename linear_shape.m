03b8f5349ed993f3aee5906c084da1711f9fb14d
mdforbes500 <mforbes4@gmu.edu>
2017-02-22 20:38:25 -0500

    Update and rename Linear_shape.m to linear_shape.m

--- linear_shape.m
+++ linear_shape.m
@@ -1,4 +1,4 @@
-function y = Linear_shape(x, x1, x2, y1, y2)
+function y = linear_shape(x, x1, x2, y1, y2)
 %LINEAR_SHAPE The linear shape function for FEA.
 %   Creates two local variables, phi1 and phi2, that represent the shape
 %   functions used in the Galerkin method of approximation. 
