a81e54d4c555af357c058365707d78a8747225dd
zliao4 <zliao4@masonlive.gmu.edu>
2017-02-24 17:59:20 -0500

    Update assembler.m

--- assembler.m
+++ assembler.m
@@ -6,16 +6,17 @@ K_e=stiffness_matrix(element);
 
 n=1/element.h_e;%n is the number of elements
 
-if(element.type_e==1)
+if(element.type_e==1)%for linear case
     K=zeros(n+1,n+1);
     for i=1:n
         K(i:i+1,i:i+1)=K(i:i+1,i:i+1)+K_e;
     end   
     
-elseif(element.type_e==2)
+elseif(element.type_e==2)%for quadratic case
     n=2*n;
     K=zeros(n+1,n+1);
     for i=1:2:n-1
         K(i:i+2,i:i+2)=K(i:i+2,i:i+2)+K_e;
     end   
+    
 end
