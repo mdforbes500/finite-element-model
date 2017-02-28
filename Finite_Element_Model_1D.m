234f9144386ab0d949c86b61d2e6630081b6f2c8
mdforbes500 <mforbes4@gmu.edu>
2017-02-28 16:45:44 -0500

    Added a script
    Added a script for implementing the functions. As can be seen in the comments, the domain may be partitioned to account for changes in the element properties. With the global assembler function (which I am in the process of rewriting), there is no need for user-input. Everything may be passed by parameter through it. Please don't gum up the code by asking the user to enter stuff. Thanks.

---
+++ Finite_Element_Model_1D.m
@@ -0,0 +1,60 @@
+% Created by Malcolm D. Forbes
+% Spring 2017
+
+clc
+clear
+close all
+
+% Script: 1D Finite Element Model
+
+% Define domain
+xA = 0; %global starting value
+xB = 1; %global ending value
+omega = linspace(xA, xB);
+length = xB - xA;
+
+%Discretize domain
+N = 10; %number of elements
+    % Define Elemental properties for each partition
+    %If there are extra partitions, create new elemental properties.
+    
+    %Partition 1
+    a = 1;
+    c = 1;
+    h = 1;
+    type = 1; %1 for linear, 2 for quadratic
+    %Construct sample element for each partition
+    element1 = Element(type, a, c, h);
+    % Fill partition matrix with partition's sample element
+    for i = 1:N
+        elements(i) = element1;
+    end
+    
+% Build elemental stiffness matrices for all elements, and store it in a
+% cell K_e for retreival in assembly.
+% Run this algorithm for each partition.
+
+    %Partition 1
+    K_e = cell(1,N);
+    for i = 1:N
+        K_e{i} = stiffness_matrix(elements(i));
+    end
+
+% Build global stiffness matrix from elemental stiffness matrices.
+%This process is independent of partitions, as all elements of the mesh
+%must be included to approximate solution.
+
+%K = assembler(K_e);
+
+% Apply boundary conditions from the problem statement.
+%Q = [];
+%F = [];
+
+% Solve for unknowns
+%U = solver(Q, F, K);
+
+% Post-processing - change answer into relevant quatities.
+
+    %Plotting data
+    %figure
+    %plot(X, U, '-ob')
