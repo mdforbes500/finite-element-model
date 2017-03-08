215b2b112caa261bdbdcdc93a729583cb52b39fe
mdforbes500 <mforbes4@gmu.edu>
2017-03-08 12:50:06 -0500

    Huzzah!

--- Finite_Element_Model_1D_Heat.m
+++ Finite_Element_Model_1D_Heat.m
@@ -18,23 +18,35 @@ A = 1 %meteres squared
 p = 4 %meters
 beta = 10 % Watts/(meter squared - K)
 k = 50.2*length/(length-A)*(1/omega); %conductance as a function along the length of the wall
-
+T_inf = 60+273.15 %Kelvins
 
 %Discretize domain
 N = 5; %number of elements
     % Define Elemental properties for each partition
     %If there are extra partitions, create new elemental properties.
     
+    k_avg = zeros(1,N);
+    x_node = zeros(1,N+1);
+    for i = 2:N+1
+        if i-1 != 0
+            x_node(i-1) = (x_node(i) -x_node(i-1))/2;
+            k_avg(i-1) = k(x_node(i-1));
+        else
+            x_node(1) = 0;
+            k_avg(1) = k(1);
+        end
+    end
+    
     %Partition 1 - Same material throughout
-    a = k*A;
+    a = k_avg*A;
     c = A*p*beta;
     h = length/N;
     type = 1; %1 for linear, 2 for quadratic
+    
     %Construct sample element for each partition
-    element1 = Element(type, a, c, h);
     % Fill partition matrix with partition's sample element
     for i = 1:N
-        elements(i) = element1;
+        elements(i) = Element(type, a(i), c, h);;
     end
     
 % Build elemental stiffness matrices for all elements, and store it in a
@@ -51,17 +63,19 @@ N = 5; %number of elements
 %This process is independent of partitions, as all elements of the mesh
 %must be included to approximate solution.
 
-K = assembler(element1.type_e,K_e);
+K = assembler(elements(1).type_e,K_e);
 
 % Apply boundary conditions from the problem statement.
-%Q = [];
-%F = [];
+
+Q = zeros(1,N+1);
+Q(N+1) = -beta*A*T_inf;
 
 % Solve for unknowns
-%U = solver(Q, F, K);
+U = solver_heat(Q, F, K);
 
 % Post-processing - change answer into relevant quatities.
 
     %Plotting data
-    %figure
-    %plot(X, U, '-ob')
+    figure
+    plot(X, U, '-ob')
+%END SCRIPT
