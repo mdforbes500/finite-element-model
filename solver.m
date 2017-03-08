23ff072fcef46cb8066621f4c0b18186becf8f12
mdforbes500 <mforbes4@gmu.edu>
2017-03-08 11:11:49 -0500

    Added Q boundary conditions
    Needed to adjust the stiffness matrix for linear dependency.

--- solver.m
+++ solver.m
@@ -5,6 +5,8 @@ switch element1.type_e
     case 1
         U = zeros(1,N+1);
         U(1) = 250 %Kelvins
+        K(N+1,N+1) = 10 + K(N+1,N+1);
+        Q(N+1, 1) = 600; %Watts
         U(2:N) = (F(2:N) + Q(2:N))/K(2:N, 2:N);
     case 2
         U = zeros(1,2*N+1);
