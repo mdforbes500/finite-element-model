d0d560015ae28819530b0ec5170d083b820dd62a
mdforbes500 <mforbes4@gmu.edu>
2017-03-08 13:24:07 -0500

    Delete solver.m

--- solver.m
+++
@@ -1,5 +0,0 @@
-function U = solver(Q, F, K)
-%SOLVER Solves the matrix equation
-% Adds boundary condition vector, Q,to force vector, F, and performs matrix multiplication on it.
-U = (F + Q)\K;
-end
