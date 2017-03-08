5c17e707e8d9183d4cdc37b8397c6ea6a194b2fc
mdforbes500 <mforbes4@gmu.edu>
2017-03-08 11:05:39 -0500

    Added boundary condition

--- solver.m
+++ solver.m
@@ -4,6 +4,7 @@ function U = solver(Q, F, K, N)
 switch element1.type_e
     case 1
         U = zeros(1,N+1);
+        U(1) = 250 %Kelvins
         U(2:N) = (F(2:N) + Q(2:N))/K(2:N, 2:N);
     case 2
         U = zeros(1,2*N+1);
