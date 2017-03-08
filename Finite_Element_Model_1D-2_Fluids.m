b1944316d9f4a37182b0e551dafd349287d836b1
mdforbes500 <mforbes4@gmu.edu>
2017-03-08 16:40:29 -0500

    Updated
    Needs more to model, but early testing.

--- Finite_Element_Model_1D-2_Fluids.m
+++ Finite_Element_Model_1D-2_Fluids.m
@@ -13,30 +13,32 @@ xB = 1; %global ending value
 omega = linspace(xA, xB);
 length = xB - xA;
 
+pressure_diff = 50*10^6; %Pa
+
 %Discretize domain
 N = 10; %number of elements
     % Define Elemental properties for each partition
     %If there are extra partitions, create new elemental properties.
     
     %Partition 1
-    a = 1;
-    c = 1;
-    h = 1;
-    type = 2; %1 for linear, 2 for quadratic
+    a1 = 1;
+    c1 = 0;
+    h1 = length/N;
+    type1 = 2; %1 for linear, 2 for quadratic
     %Construct sample element for each partition
-    element1 = Element(type, a, c, h);
+    element1 = Element(type1, a1, c1, h1);
     % Fill partition matrix with partition's sample element
     for i = 1:N/2
         elements(i) = element1;
     end
     
     %Partition 2
-    a = 1;
-    c = 1;
-    h = 1;
-    type = 2; %1 for linear, 2 for quadratic
+    a2 = 1;
+    c2 = 0;
+    h2 = length/N;
+    type2 = 2; %1 for linear, 2 for quadratic
     %Construct sample element for each partition
-    element2 = Element(type, a, c, h);
+    element2 = Element(type2, a2, c2, h2);
     % Fill partition matrix with partition's sample element
     for i = N/2+1:N
         elements(i) = element2;
@@ -56,17 +58,38 @@ N = 10; %number of elements
 %This process is independent of partitions, as all elements of the mesh
 %must be included to approximate solution.
 
-K = assembler(element1.type_e,K_e);
+if (element1.type_e ~= element2.type_e)
+    error('Element types do not match. Please revise.')
+else
+    K = assembler(element1.type_e,K_e);
+end
 
 % Apply boundary conditions from the problem statement.
-%Q = [];
-%F = [];
+Q = zeros(1,2*N+1);
+Q(1) = pressure_diff/length;
+Q(2*N+1) = -pressure_diff/length;
+
+% Elemental force vector 
+F_e = cell(1,2*N+1);
+for i = 1:2*N+1
+    F_e{i} = pressure_diff*h/6*.[1;4;1];
+end
+
+%Assembling the global force vector
+F = zeros(1,2*N+1);
+F(1) = F_e{1}(1);
+F(2) = F_e{1}(2);
+F(2*N) = F_e{N}(2);
+F(2*N+1) = F_e{N}(3);
+for i = 3:2*N-1
+    F(i) = F_e{i-2}(i)+F_e{i-1}(i-2);
+end
 
 % Solve for unknowns
-%U = solver(Q, F, K);
+U = solver(Q, F, K);
 
 % Post-processing - change answer into relevant quatities.
 
     %Plotting data
-    %figure
-    %plot(X, U, '-ob')
+    figure
+    plot(X, U, '-ob')
