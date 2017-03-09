6ecfed8cecae2a8b3c29a3f40042550b5627eb46
zliao4 <zliao4@masonlive.gmu.edu>
2017-03-09 12:27:39 -0500

    Update assembler.m

--- assembler.m
+++ assembler.m
@@ -12,8 +12,10 @@ switch type
     
     case 2 %for quadratic case
      K=zeros(2*length(K_e)+1,2*length(K_e)+1);
-        for i=1:2:(length(K_e)-1)
+     m=1;
+        for i=1:1:length(K_e)
             %assemble elemental matrix
-            K(i:i+2,i:i+2)=K(i:i+2,i:i+2)+K_e{i};
+            K(m:m+2,m:m+2)=K(m:m+2,m:m+2)+K_e{i};
+            m=m+2;
         end  
 end
