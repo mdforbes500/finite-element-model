2545d0f62dff253a3502093ec2b8fde852465aca
mdforbes500 <mforbes4@gmu.edu>
2017-03-20 08:02:34 -0400

    Update Finite_Element_Model_1D-2_Fluids.m

--- Finite_Element_Model_1D-2_Fluids.m
+++ Finite_Element_Model_1D-2_Fluids.m
@@ -8,39 +8,42 @@ close all
 % Script: 1D Finite Element Model
 
 % Define domain
+RES = 999; %resolution of approximation
 xA = 0; %global starting value
 xB = 1; %global ending value
-omega = linspace(xA, xB);
+omega = xA: 1/RES: xB;
 length = xB - xA;
 
-pressure_diff = 50*10^6; %Pa
+f_0 = 50; %Pa
+mu_water = 1.12;
+mu_lubricant = 0.3;
 
 %Discretize domain
-N = 10; %number of elements
+N = 50; %number of elements
     % Define Elemental properties for each partition
     %If there are extra partitions, create new elemental properties.
     
-    %Partition 1
-    a1 = 1;
+    %Partition 1 - the water in lower region
+    a1 = mu_water;
     c1 = 0;
     h1 = length/N;
-    type1 = 2; %1 for linear, 2 for quadratic
+    type1 = 1; %1 for linear, 2 for quadratic
     %Construct sample element for each partition
     element1 = Element(type1, a1, c1, h1);
     % Fill partition matrix with partition's sample element
-    for i = 1:N/2
+    for i = 1:(N/2)
         elements(i) = element1;
     end
     
     %Partition 2
-    a2 = 1;
+    a2 = mu_lubricant;
     c2 = 0;
     h2 = length/N;
-    type2 = 2; %1 for linear, 2 for quadratic
+    type2 = 1; %1 for linear, 2 for quadratic
     %Construct sample element for each partition
     element2 = Element(type2, a2, c2, h2);
     % Fill partition matrix with partition's sample element
-    for i = N/2+1:N
+    for i = N/2:N
         elements(i) = element2;
     end
     
@@ -51,7 +54,7 @@ N = 10; %number of elements
     %Partition 1
     K_e = cell(1,N);
     for i = 1:N
-        K_e{i} = stiffness_matrix(elements(i))
+        K_e{i} = stiffness_matrix(elements(i));
     end
 
 % Build global stiffness matrix from elemental stiffness matrices.
@@ -65,31 +68,61 @@ else
 end
 
 % Apply boundary conditions from the problem statement.
-Q = zeros(1,2*N+1);
-Q(1) = pressure_diff/length;
-Q(2*N+1) = -pressure_diff/length;
+Q = zeros(N+1,1);
+%Q(1) = f_0/length;
+%Q(2*N+1) = -f_0/(length;
 
 % Elemental force vector 
-F_e = cell(1,2*N+1);
-for i = 1:2*N+1
-    F_e{i} = pressure_diff*h/6*.[1;4;1];
+F_e = cell(1,N+1);
+for i = 1:N+1
+    F_e{i} = f_0*h1/2.*[1;1];
 end
 
 %Assembling the global force vector
-F = zeros(1,2*N+1);
+F = zeros(N+1,1);
 F(1) = F_e{1}(1);
-F(2) = F_e{1}(2);
-F(2*N) = F_e{N}(2);
-F(2*N+1) = F_e{N}(3);
-for i = 3:2*N-1
-    F(i) = F_e{i-2}(i)+F_e{i-1}(i-2);
+F(N+1) = F_e{N}(2);
+e = 1;
+for i = 2:N
+    F(i) = F_e{e}(2)+F_e{e+1}(1);
+    e = e+1;
 end
 
 % Solve for unknowns
-U = solver(Q, F, K);
+U = solver(element1, Q, F, K, N);
 
 % Post-processing - change answer into relevant quatities.
+x_e = zeros(1, N+1);
+for i = 2:N+1
+    x_e(i) = x_e(i-1)+h1;
+end
+
+for i = 1:500
+    if N == 1
+        u1(i) = quadratic_shape(omega(i),x_e(1),x_e(2), x_e(3), U(1), U(2), U(3));
+    else
+        for j = 2:N
+            u1(i) = quadratic_shape(omega(i),x_e(j-1),x_e(j), x_e(j+1),U(j-1), U(j), U(j+1));
+        end
+    end
+end 
+for i = 1:500
+    if N == 1
+        u2(i) = quadratic_shape(omega(i),x_e(3),x_e(4), x_e(5), U(3), U(4), U(5));
+    else
+        for j = 2:N
+            u2(i) = quadratic_shape(omega(i),x_e(j+1),x_e(j), x_e(j-1),U(j+1), U(j), U(j-1));
+        end
+    end
+end 
 
     %Plotting data
     figure
-    plot(X, U, '-ob')
+    hold on
+    %plot(omega(1:500), u1, '-b')
+    %plot(omega(501:1000), u2, '-b')
+    title('Velocity profile of two fluids')
+    xlabel('y')
+    ylabel('u(y)')
+    plot (x_e, U, '-ob')
+    hold off
